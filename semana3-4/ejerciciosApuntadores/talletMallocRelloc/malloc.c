af583d0c3a8aacc686679bcee84a5c868cc7a0fd
JORGE H SiERRA L <jorgesierralaiton@gmail.com>
2024-10-19 14:02:51 -0500

    Update and rename semana3-4/ejerciciosApuntadores/apuntadoresDobles1.c to semana3-4/ejerciciosApuntadores/talletMallocRelloc/malloc.c
    correcion para el taller de malloc

--- semana3-4/ejerciciosApuntadores/talletMallocRelloc/malloc.c
+++ semana3-4/ejerciciosApuntadores/talletMallocRelloc/malloc.c
@@ -2,7 +2,7 @@
 autor: jorge Sierra
 materia; Sistemas Operativos
 fecha: 8/08/2024
-tema: doble apuntadores
+tema: Uso de malloc en codigo de c, el cual se usa a traves de apuntadores
  ********************************************************************************************/
 
 #include <string.h>
@@ -10,17 +10,23 @@ tema: doble apuntadores
 #include <stdio.h>
 
 main(){
+
+  // se reserva la memoria de 4 numeros enteros de distintas maneras
   int *varptr1 = malloc(4*sizeof(int));
   int *varptr2 = malloc(sizeof(int[4]));
   int *varptr3 = malloc(4*sizeof *varptr3);
   
   if(varptr1){
+    //se rellena ese espacio de 4 numeros 
     for(int i =0 ;i<4; i++){
       varptr1[i]=i*i;
     }
+    // se imprimen los 4 numeros guardados 
     for(int i =0 ;i<4; i++){
       printf("varptr1[%d]\t == %d\n",i, varptr1[i]);
     }
+
+    //se libera la memoria de los 3 apuntadores
     free(varptr1);
     free(varptr2);
     free(varptr3);
