fd74707805172911c53d1eaffd1b55fda8d3f664
JORGE H SiERRA L <jorgesierralaiton@gmail.com>
2024-10-19 17:15:57 -0500

    cambio de carpeta y se agrega los comentarios

--- semana6-13/tallerPipeYFork/pipeServidor.c
+++ semana6-13/tallerPipeYFork/pipeServidor.c
@@ -2,16 +2,16 @@
 autor: jorge Sierra
 materia; Sistemas Operativos
 fecha: 3/10/2024
-tema: FIFO, servidor como el que escucha los mensajes
+tema: FIFO, servidor como el que escucha los mensajes y solo lee de la tuberia
  ********************************************************************************************/
     
 
-  #include <stdio.h>
-  #include <sys/stat.h>
-  #include <unistd.h>
-  #include <sys/types.h>
-  #include <string.h> 
-  #include <fcntl.h>
+ #include <stdio.h>
+ #include <sys/stat.h>
+ #include <unistd.h>
+ #include <sys/types.h>
+ #include <string.h> 
+ #include <fcntl.h>
  #define FIFO_FILE "MYFIFO"
 
 int main() {
@@ -21,16 +21,17 @@ int main() {
      int to_end;
      int read_bytes;
     
-     /* Create the FIFO if it does not exist */
+     // crea la tuberia
      mknod(FIFO_FILE, S_IFIFO|0640, 0);
      strcpy(end, "end");
+ 
      while(1) {
-        fd = open(FIFO_FILE, O_RDONLY);
-        read_bytes = read(fd, readbuf, sizeof(readbuf));
-        readbuf[read_bytes] = '\0';
+        fd = open(FIFO_FILE, O_RDONLY); // abre la tuberia solo para lectura
+        read_bytes = read(fd, readbuf, sizeof(readbuf)); // lee la tuberia
+        readbuf[read_bytes] = '\0'; //agrega el \0 para crear el string
         printf("Received string: \"%s\" and length is %d\n", readbuf, (int)strlen    (readbuf));
         to_end = strcmp(readbuf, end);
-        if (to_end == 0) {
+        if (to_end == 0) { // si readbuf es igual a end se terminara y se cerrara la tuebria
            close(fd);
            break;
         }
